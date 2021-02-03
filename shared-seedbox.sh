c70d2441d1ed6ef69f75dff51421772379ee506f
jerry048 <jw950@jerry048.com>
2021-02-03 11:36:58 +0800

    Update shared-seedbox.sh

--- shared-seedbox.sh
+++ shared-seedbox.sh
@@ -4,7 +4,8 @@
 tokens=$1
 username=$2
 password=$3
-port=$4
+qbport=$4
+publicip=$(curl https://ipinfo.io/ip)
 
 # Downloading Components
 curl -s -O https://$tokens@raw.githubusercontent.com/jerry048/Seedbox-Install-Components/main/.seedbox_installation.sh
@@ -46,9 +47,10 @@ Connection\PortRangeMin=45000
 Downloads\SavePath=$HOME/qbittorrent/Downloads/
 Queueing\QueueingEnabled=false
 WebUI\Password_PBKDF2="@ByteArray($PBKDF2password)"
-WebUI\Port=$port
+WebUI\Port=$qbport
 WebUI\Username=$username
 EOF
+        rm $HOME/qb_password_gen
     fi
 }
 
@@ -76,7 +78,6 @@ do
         *) tput setaf 1; echo "Please choose a valid version";;
     esac
 done
-
 
 # qBittorrent local user servive
 if [ "${e}" == "0" ]; then
@@ -111,8 +112,98 @@ elif [ "${e}" == "2" ]; then
     $HOME/bin/qbittorrent-nox -d
 fi
 
+if [ ! $? -eq 0 ]; then
+    echo "qBittorrent installation failed, try another method"
+    rm $HOME/bin/qbittorrent-nox
+    rm -r $HOME/.config/qBittorrent
+    exit 1
+fi
+
+# autoremove-torrents
+Decision2 qBittorrent
+if [ "${e}" == "0" ]; then
+    tput setaf 2
+    read -p "Enter desired reserved storage (in GiB): " diskspace
+    read -p "Enter desired minimum seedtime (in Second): " seedtime
+    tput setaf 2; echo "How to install qBittorrent:"
+    options=("pip" "python3")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "pip")
+                e=0; break
+                ;;
+            "python3")
+                e=1; break
+                ;;
+            *) tput setaf 1; echo "Please choose a valid version";;
+        esac
+    done
+    if [ "${e}" == "0" ]; then
+        pip install autoremove-torrents
+    elif [ "${e}" == "1" ]; then
+        git clone https://github.com/jerrymakesjelly/autoremove-torrents.git
+        cd autoremove-torrents
+        python3 setup.py install --prefix $HOME/.local/
+    fi
+    if [ ! $? -eq 0 ]; then
+        echo "autoremove-torrents installation failed"
+        rm -r autoremove-torrents
+        rm $HOME/.local/bin/autoremove-torrents
+        exit 1
+    fi
+    touch $HOME/.config.yml
+    cat << EOF >>$HOME/.config.yml
+General-qb:          
+  client: qbittorrent
+  host: http://127.0.0.1:$qbport
+  username: $username
+  password: $password
+  strategies:
+    Upload:
+      status:
+        - Uploading
+      remove: upload_speed < 1024 and seeding_time > $seedtime
+    Leech:
+      status:
+        - Downloading
+      remove: ratio < 1 and progress > 10 and download_speed > 20480
+    Disk:
+      free_space:
+        min: $diskspace
+        path: $HOME/
+        action: remove-old-seeds
+  delete_data: true
+M-Team-qb:          
+  client: qbittorrent
+  host: http://127.0.0.1:$qbport
+  username: $username
+  password: $password
+  strategies:
+    Ratio:
+      trackers:
+        - tracker.m-team.cc
+      upload_ratio: 3
+  delete_data: true
+EOF
+    sed -i 's+127.0.0.1: +127.0.0.1:+g' $HOME/.config.yml
+    mkdir $HOME/.autoremove-torrents
+    chmod 777 $HOME/.autoremove-torrents
+    touch $HOME/.autoremove.sh
+    cat << EOF >$HOME/.autoremove.sh
+#!/bin/sh
+
+while true; do
+  $HOME/.local/bin/autoremove-torrents --conf=$HOME/.config.yml --log=$HOME/.autoremove-torrents
+  sleep 5
+done
+EOF
+    chmod +x $HOME/.autoremove.sh
+    screen -dmS autoremove-torrents $HOME/.autoremove.sh
+fi
+
+
 # Cleanup
 rm $HOME/.seedbox_installation.sh
-rm $HOME/qb_password_gen
 clear
-echo "qBittorrent $version is sucessfully installed"
+echo "qBittorrent $version is sucessfully installed, visit at $publicip:$qbport"
