1375d88f3d63d6399cda3ec8ecb4764161520d3b
jerry048 <61510650+jerry048@users.noreply.github.com>
2021-02-21 23:31:15 +0800

    Add files via upload

---
+++ Bandwidth_Usage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+## Grab interface name
+interface=$(ip -o -4 route show to default | awk '{print $5}')
+
+## Define Monitor_Network_Bandwidth
+function Monitor_Network_Bandwidth {
+	rx=$(sar -n DEV 1 1 | grep $interface | grep Average | awk '{print $5}')
+	rx=$(echo ${rx%.*})
+	rx=$(expr $rx \* 125 / 131072)
+
+	tx=$(sar -n DEV 1 1 | grep $interface | grep Average | awk '{print $6}')
+	tx=$(echo ${tx%.*})
+	tx=$(expr $tx \* 125 / 131072)
+}
+
+while true; do
+	tput cup 0 0
+	Monitor_Network_Bandwidth
+	echo "Bandwidth Usage of $interface"
+    printf "%10s %6d %3s   \n" \
+      "Incoming:" $rx "MiB"\
+      "Outgoing:" $tx "MiB"
+done
